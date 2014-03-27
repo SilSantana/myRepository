af2185a1ba1da3a0cd7c996c5be6ff468f6d7e3a
SilSantana <sil_s_s@yahoo.com.br>
2014-03-27 11:22:01 +0800

    My App
    List the names

---
+++ GitApp/GitApp/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lstNames = New System.Windows.Forms.ListBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(26, 31)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(88, 13)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Input your Name:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(124, 27)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(192, 20)
+        Me.txtName.TabIndex = 1
+        '
+        'lstNames
+        '
+        Me.lstNames.FormattingEnabled = True
+        Me.lstNames.Items.AddRange(New Object() {"Silvana"})
+        Me.lstNames.Location = New System.Drawing.Point(29, 75)
+        Me.lstNames.Name = "lstNames"
+        Me.lstNames.Size = New System.Drawing.Size(277, 121)
+        Me.lstNames.TabIndex = 2
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(328, 25)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(76, 21)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Send"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(452, 321)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.lstNames)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents lstNames As System.Windows.Forms.ListBox
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+
+End Class
---
+++ GitApp/GitApp/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    ' Author: Silvana  V1.0
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        If Not txtName.Text = "" Then
+            lstNames.Items.Add(txtName.Text)
+        End If
+
+        txtName.Text = ""
+    End Sub
+End Class
